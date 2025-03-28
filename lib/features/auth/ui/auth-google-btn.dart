3afb9cda27689c83c19b24f9d19df29fbc9dc490
toothlessdev <toothless042@gmail.com>
2025-03-28 16:52:03 +0900

    feature: 생체인증 로그인, 구글 로그인 버튼 위젯 구현

---
+++ lib/features/auth/ui/auth-google-btn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthGoogleButton extends StatelessWidget {
+  const AuthGoogleButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
